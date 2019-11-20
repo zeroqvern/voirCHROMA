ffea13a401b5e9b40e961fbb8fff95cb2494ab90
AshVern <45629815+zeroqvern@users.noreply.github.com>
2019-11-20 23:14:29 +0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://Vern:123qwer456@clustertest-ftjcv.gcp.mongodb.net/voirChroma?retryWrites=true&w=majority',
-    apiKey: 'acc_a28e5bccaf9a2d1',
-    apiSecret: '9327ffbbfb8ce04ca58773550561ded0'
-};
